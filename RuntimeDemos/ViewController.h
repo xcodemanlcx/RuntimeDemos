46388529a6c9165c1e565a7f0658952c9cf2c71b
xcodemanlcx <475512103@qq.com>
2019-04-17 09:24:51 +0800

    init

---
+++ RuntimeDemos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeDemos
+//
+//  Created by leichunxiang on 2019/4/17.
+//  Copyright © 2019 lcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RuntimeDemos/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RuntimeDemos
+//
+//  Created by leichunxiang on 2019/4/17.
+//  Copyright © 2019 lcx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
